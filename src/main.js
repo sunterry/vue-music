a741099eb583b79f58f9d235ba1b270753d4e70f
段通 <15253130515@163.com>
2019-03-09 23:06:07 +0800

    app-music created

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import FastClick from 'fastclick';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+FastClick.attach(document.body);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
